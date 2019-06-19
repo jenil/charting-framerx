2650bbe99eda1b15f6adaaf5f205e9cf779433b6
Jenil Gogari <jenil27@hotmail.com>
2019-06-19 16:26:43 -0400

    inital commit 🎉

---
+++ Charting.framerfx/code/Charting.tsx
@@ -0,0 +1,7 @@
+import { addPropertyControls, Color, ControlType, Frame } from "framer";
+import * as React from "react";
+import Chart from "react-apexcharts";
+
+export const Charting = function(props) {
+  return <h1>Charting</h1>;
+};
